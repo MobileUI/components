cdefd3aadb7ab82779228b032689fc63cb95efe9
Fábio Rogério <fabio.rogerio@tecnospeed.com.br>
2017-05-16 14:13:29 -0300

    add tab component #21

---
+++ tab/script.js
@@ -0,0 +1,13 @@
+window.openTab = function(t){
+  var button = event.target;
+  var tabContent = document.getElementById(t).parentNode.getElementsByClassName('tab-content');
+  var buttonActived = button.parentNode.getElementsByClassName('active');
+  for (var i = 0; i < tabContent.length; i++) {
+    tabContent[i].setAttribute('class',tabContent[i].getAttribute('class').replace('active',''));
+  }
+  for (var i = 0; i < buttonActived.length; i++) {
+    buttonActived[i].setAttribute('class',buttonActived[i].getAttribute('class').replace('active',''));
+  }
+  button.setAttribute('class', button.getAttribute('class') + ' active');
+  document.getElementById(t).setAttribute('class', document.getElementById(t).getAttribute('class') + ' active');
+}
