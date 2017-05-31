46d66308b3ab91e05d593f99aaf4ebf38e4a2c77
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2017-05-30 22:52:00 -0300

    add function formatMoney mobileuijs

--- mobileuijs/script.js
+++ mobileuijs/script.js
@@ -54,6 +54,18 @@
       if(index) {
         html = html.replace(new RegExp('\\$\\$index', 'g'), index);
       }
+      //valid funcionBind
+      //TODO: Improve performance and logic
+      var functionBind = html.split('$$');
+      for (var i = 1; i < functionBind.length; i++) {
+        var prop = functionBind[i].split('{')[1].split(',')[0].split('}')[0];
+        var value = fetchFromObject(dataObj,prop);
+        var functionName = functionBind[i].split('{')[0];
+        var functionReplace = functionBind[i].split('}')[0] + '}';
+        var functionInvoke = 'window.MobileUI.' + functionBind[i].split('}')[0].replace(prop, "'"+value+"'").replace('{','(') + ')';
+        var valueBind = eval(functionInvoke);
+        html = html.replace(new RegExp('\\$\\$'+functionReplace, 'g'), valueBind);
+      }
       var divBind = document.createElement('div');
       elm.parentNode.insertBefore(divBind, elm);
       html = html.replace('data=','data-binded=');
@@ -214,6 +226,15 @@
     },
     focus: function(id){
       document.getElementById(id).focus();
+    },
+    formatMoney: function(n, c, d, t){
+      var c = isNaN(c = Math.abs(c)) ? 2 : c,
+      d = d == undefined ? "." : d,
+      t = t == undefined ? "," : t,
+      s = n < 0 ? "-" : "",
+      i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+      j = (j = i.length) > 3 ? j % 3 : 0;
+     return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
     },
     ajax: superagent
   }
