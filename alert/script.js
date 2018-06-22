b03bf8a21a2e0cacd281c57c31a7d35f2db5b00b
Fabio Rogeiro <fabio@tindin.com.br>
2018-06-22 11:49:57 -0300

    add closeAlert with id, thanks @dotenorio

--- alert/script.js
+++ alert/script.js
@@ -1,86 +1,88 @@
-window.alert = function(message, title){
-  var configAlert = {};
-  if(typeof message === "object"){
-    configAlert = message;
+window.alert = function (message, title) {
+  var configAlert = {}
+  if (typeof message === 'object') {
+    configAlert = message
   } else {
-    configAlert.message = message;
-    configAlert.title = title;
+    configAlert.message = message
+    configAlert.title = title
   }
-  if(!configAlert.id){
-    configAlert.id = 'ALERT'+new Date().getTime();
+  if (!configAlert.id) {
+    configAlert.id = 'ALERT' + new Date().getTime()
   }
-  if(!configAlert.buttons || !configAlert.buttons.length) {
+  if (!configAlert.buttons || !configAlert.buttons.length) {
     configAlert.buttons = [
       {
         label: 'OK',
-        onclick: function(){
-          closeAlert();
+        onclick: function () {
+          closeAlert()
         }
       }
-    ];
+    ]
   }
-  var body = document.getElementsByTagName('body')[0];
-  if(event && event.target && event.target.parentNode && event.target.parentNode.className.indexOf('body') >= 0) {
-    body = event.target.parentNode;
-  }
-
-  var e = document.createElement('div');
-  e.className = 'backdrop show backdrop-alert';
-  e.id = configAlert.id + '_BACKDROP';
-  body.appendChild(e);
-
-  var alertMobileUI = document.createElement('div');
-  alertMobileUI.className = 'alert-mobileui';
-  alertMobileUI.id = configAlert.id;
-  e.parentNode.appendChild(alertMobileUI);
-
-  var alertContent = document.createElement('div');
-  if(!configAlert.class){
-    configAlert.class = 'white';
-  }
-  alertContent.className = 'alert ' + configAlert.class;
-  if(configAlert.width) {
-    alertContent.style.maxWidth = configAlert.width;
-  }
-  alertMobileUI.appendChild(alertContent);
-
-  if(configAlert.title){
-    var h1 = '<h1>'+configAlert.title+'</h1>';
-    alertContent.insertAdjacentHTML('beforeend', h1);
-  }
-  if(configAlert.message){
-    var p = '<p>'+configAlert.message+'</p>';
-    alertContent.insertAdjacentHTML('beforeend', p);
-  }
-  if(configAlert.template) {
-    alertContent.insertAdjacentHTML('beforeend', document.getElementById(configAlert.template).innerHTML);
+  var body = document.getElementsByTagName('body')[0]
+  if (event && event.target && event.target.parentNode && event.target.parentNode.className.indexOf('body') >= 0) {
+    body = event.target.parentNode
   }
 
-  var buttons = document.createElement('div');
-  buttons.className = 'buttons';
-  alertContent.appendChild(buttons);
+  var e = document.createElement('div')
+  e.className = 'backdrop show backdrop-alert'
+  e.id = configAlert.id + '_BACKDROP'
+  body.appendChild(e)
 
-  for(var i in configAlert.buttons){
-    var button = document.createElement('button');
-    if(!configAlert.buttons[i].class){
-      configAlert.buttons[i].class = 'text-teal';
+  var alertMobileUI = document.createElement('div')
+  alertMobileUI.className = 'alert-mobileui'
+  alertMobileUI.id = configAlert.id
+  e.parentNode.appendChild(alertMobileUI)
+
+  var alertContent = document.createElement('div')
+  if (!configAlert.class) {
+    configAlert.class = 'white'
+  }
+  alertContent.className = 'alert ' + configAlert.class
+  if (configAlert.width) {
+    alertContent.style.maxWidth = configAlert.width
+  }
+  alertMobileUI.appendChild(alertContent)
+
+  if (configAlert.title) {
+    var h1 = '<h1>' + configAlert.title + '</h1>'
+    alertContent.insertAdjacentHTML('beforeend', h1)
+  }
+  if (configAlert.message) {
+    var p = '<p>' + configAlert.message + '</p>'
+    alertContent.insertAdjacentHTML('beforeend', p)
+  }
+  if (configAlert.template) {
+    alertContent.insertAdjacentHTML('beforeend', document.getElementById(configAlert.template).innerHTML)
+  }
+
+  var buttons = document.createElement('div')
+  buttons.className = 'buttons'
+  alertContent.appendChild(buttons)
+
+  for (var i in configAlert.buttons) {
+    var button = document.createElement('button')
+    if (!configAlert.buttons[i].class) {
+      configAlert.buttons[i].class = 'text-teal'
     }
-    button.className = configAlert.buttons[i].class;
-    var text = document.createTextNode(configAlert.buttons[i].label);
-    button.appendChild(text);
-    if(!configAlert.buttons[i].onclick) {
-      configAlert.buttons[i].onclick = closeAlert;
+    button.className = configAlert.buttons[i].class
+    var text = document.createTextNode(configAlert.buttons[i].label)
+    button.appendChild(text)
+    if (!configAlert.buttons[i].onclick) {
+      configAlert.buttons[i].onclick = closeAlert
     }
-    button.addEventListener('click', configAlert.buttons[i].onclick);
-    buttons.appendChild(button);
+    button.addEventListener('click', configAlert.buttons[i].onclick)
+    buttons.appendChild(button)
   }
-
 }
 
-window.closeAlert = function(){
-  var alertId = event.target.parentNode.parentNode.parentNode.id;
-  var alert = document.getElementById(alertId);
-  alert.parentNode.removeChild(alert);
-  var backdrop = document.getElementById(alertId+'_BACKDROP');
-  backdrop.parentNode.removeChild(backdrop);
+window.closeAlert = function (id) {
+  alertId = id
+  if (!alertId) {
+    alertId = event.target.parentNode.parentNode.parentNode.id
+  }
+  var alert = document.getElementById(alertId)
+  alert.parentNode.removeChild(alert)
+  var backdrop = document.getElementById(alertId + '_BACKDROP')
+  backdrop.parentNode.removeChild(backdrop)
 }
